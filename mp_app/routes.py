38c52bc72c3e97036494aa12aabf2f82670a58d0
m0no <m0nochr0mex@gmail.com>
2022-09-02 14:02:36 +0800

    submitData route initial implementation

--- mp_app/routes.py
+++ mp_app/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Response, HTTPException
+from fastapi import APIRouter, Response, HTTPException, UploadFile
+from beanie import WriteRules
 from .models import *
 
 router = APIRouter(tags=['MountainPass'])
@@ -9,4 +10,20 @@ async def root():
     return {'message': 'OK'}
 
 
+@router.post('/submitData')
+async def submit_data(data: MountainPass, photos: list[UploadFile] | None = None):
+    """
+    Receive MountainPass data and store it in DB
+    """
 
+    person_db = await Person.get_by_email(data.person.email)
+    if person_db:
+        data.person = person_db
+
+    if data.status and data.status != Status.NEW:
+        return HTTPException(400, 'Invalid status')
+
+    # TODO: Process photos
+
+    await data.save(link_rule=WriteRules.WRITE)
+    return Response(status_code=201)
